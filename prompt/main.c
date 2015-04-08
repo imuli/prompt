abd0e2584ce8ede8db09b3cea83aca60e4244156
Imuli <i@imu.li>
2015-04-08 14:10:00 -0400

    split into newline and prompt: prompt!

--- prompt/main.c
+++ prompt/main.c
@@ -10,11 +10,8 @@
 
 #include "buffer.h"
 #include "termios.h"
-#include "rune.h"
-#include "text.h"
-#include "editor.h"
 
-const int debug = 1;
+const int debug = 0;
 
 char *argv0;
 struct termios term_orig;
@@ -53,6 +50,37 @@ block(int fd){
 }
 
 static int
+fork_newline(int *infd, int *linefd, int *outfd, int pty){
+	int pipes[6];
+	int i;
+	for(i=0;i<6;i+=2)
+		if(pipe(pipes+i) < 0) return 0;
+	switch(fork()){
+	case -1:
+		return 0;
+	case 0:
+		/* even for reading, odd for writing */
+		dup2(pipes[0], 0);
+		dup2(pipes[3], 1);
+		dup2(pipes[5], 2);
+		close(pty);
+		for(i=0;i<6;i++)
+			close(pipes[i]);
+		execlp("bin/newline", "newline", "inf", NULL);
+		return 0;
+	default:
+		/* even for reading, odd for writing */
+		close(pipes[0]);
+		close(pipes[3]);
+		close(pipes[5]);
+		*infd = pipes[1];
+		*linefd = pipes[2];
+		*outfd = pipes[4];
+		return 1;
+	}
+}
+
+static int
 update_termios(int pty, struct termios* termp){
 	struct termios termq;
 	if(tcgetattr(pty, &termq)!=0) return -1;
@@ -68,6 +96,9 @@ enum {
 	Stdin = 0,
 	Stdout,
 	Pty,
+	NLin,
+	NLline,
+	NLout,
 	Npoll,
 	Bufsz = 1024,
 };
@@ -76,65 +107,92 @@ int
 loop(int pty){
 	int i, stop = 0;
 	int edit = 0;
-	Editor e;
+	int nlin, nlout, nlline;
 
 	struct termios termp = term_orig;
 	cfmakeraw(&termp);
 	termp.c_lflag &= ~ECHO;
 	if(tcsetattr(0, TCSANOW, &termp)!=0) error("tcsetattr");
 
-	Buffer in, out;
+	Buffer in, out, nl;
 	in = buffer_alloc();
 	out = buffer_alloc();
-	if(in == NULL || out == NULL)
+	nl = buffer_alloc();
+	if(in == NULL || out == NULL || nl == NULL)
 		error("buffer_alloc");
 
-	if((e = editor_init(in, out)) == NULL)
-		error("editor_init");
+	if(!fork_newline(&nlin, &nlline, &nlout, pty)) error("fork newline");
 
 	struct pollfd pfd[Npoll];
 	pfd[Stdin].fd = 0;
 	pfd[Stdout].fd = 1;
 	pfd[Pty].fd = pty;
+	pfd[NLin].fd = nlin;
+	pfd[NLline].fd = nlline;
+	pfd[NLout].fd = nlout;
 
 	unblock(0);
 	unblock(1);
 	unblock(pty);
+	unblock(nlin);
+	unblock(nlline);
+	unblock(nlout);
+
+	/* raw mode:
+	 * stdin  → ptm
+	 * stdout ← ptm
+	 *
+	 * canonical mode:
+	 * stdin → nlin
+	 * stdout ← nlout
+	 * stdout ← ptm
+	 * nlline → ptm 
+	 */
 
 	while(!stop){
 		for(i=0;i<Npoll;i++){
 			pfd[i].events = 0;
 		}
-		if(buffer_amount(in))	pfd[Pty].events |= POLLOUT;
+
+		if(buffer_space(nl))	pfd[NLline].events |= POLLIN;
 		if(buffer_space(in))	pfd[Stdin].events |= POLLIN;
+		if(buffer_space(out))	pfd[NLout].events |= POLLIN;
 		if(buffer_space(out))	pfd[Pty].events |= POLLIN;
+
+		if(buffer_amount(nl))	pfd[Pty].events |= POLLOUT;
+		if(buffer_amount(in))	pfd[Pty].events |= POLLOUT;
+		if(buffer_amount(in))	pfd[NLin].events |= POLLOUT;
 		if(buffer_amount(out))	pfd[Stdout].events |= POLLOUT;
 
 		poll(pfd, Npoll, -1);
 
-		if(pfd[Pty].revents & POLLIN){
-			buffer_pull(out, pty);
-			update_termios(pty, &termp);
-			edit = termp.c_lflag & ICANON;
-			e->echo = termp.c_lflag & ECHO;
-		}
-		if(pfd[Stdout].revents & POLLOUT){
-			buffer_push(1, out);
-		}
+		update_termios(pty, &termp);
+		edit = termp.c_lflag & ICANON;
 
-		if(pfd[Stdin].revents & POLLIN){
-			if(edit)
-				editor(e, 0);
-			else
-				buffer_pull(in, 0);
-		}
-		if(pfd[Pty].revents & POLLOUT){
+		if(pfd[Pty].revents & POLLIN)
+			buffer_pull(out, pty);
+		if(pfd[NLout].revents & POLLIN)
+			buffer_pull(out, nlout);
+		if(pfd[Stdin].revents & POLLIN)
+			buffer_pull(in, 0);
+		if(pfd[NLline].revents & POLLIN)
+			buffer_pull(nl, nlline);
+
+		if(pfd[Stdout].revents & POLLOUT)
+			buffer_push(1, out);
+		if(pfd[Pty].revents & POLLOUT)
+			buffer_push(pty, nl);
+		if(pfd[NLin].revents & POLLOUT && edit)
+			buffer_push(nlin, in);
+		if(pfd[Pty].revents & POLLOUT && !edit)
 			buffer_push(pty, in);
-		}
 
 		stop |= pfd[Stdin].revents & POLLHUP;
 		stop |= pfd[Stdout].revents & POLLHUP;
 		stop |= pfd[Pty].revents & POLLHUP;
+		stop |= pfd[NLin].revents & POLLERR;
+		stop |= pfd[NLline].revents & POLLERR;
+		stop |= pfd[NLout].revents & POLLERR;
 	}
 
 	/* flush output buffer */
