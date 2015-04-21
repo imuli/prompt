9850c5c6c88f34a7c56dfc8c01f825459d64a38c
Imuli <i@imu.li>
2015-04-21 13:38:44 -0400

    split off librune entirely

--- librune/runes_utf8s.c
+++
@@ -1,9 +0,0 @@
-#include <rune.h>
-int runes_utf8s(Runes* r, char *u){
-	int n = 0, i = r->c;
-	for(;u[n]!=0;i++){
-		n += rune_utf8(r->r+i, u+n);
-	}
-	r->c = i;
-	return i;
-}
--- librune/runeschars.c
+++
@@ -1,8 +0,0 @@
-#include <rune.h>
-int runeschars(Runes* r){
-	int n = 0, i;
-	for(i=r->c;i>0;i--){
-		n += runechar(r->r[i]);
-	}
-	return n;
-}
